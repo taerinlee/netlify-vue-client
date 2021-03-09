cc98e7f1c71e15c50ab85bbdd7164e57fe2887a0
Taerin Lee <taerin@favoritemedium.net>
2021-03-09 15:00:10 +0900

    add production option

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? process.env.VUE_PRODUCT_APP_API : process.env.VUE_APP_API,
+  baseURL: process.env.NODE_ENV !== 'localhost' ? 'https://netlify-mevn-server.netlify.app' : process.env.VUE_APP_API,
   withCredentials: true
 })
 
